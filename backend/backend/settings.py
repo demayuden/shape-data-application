de9415d20a1dd2e8a5f9e34a898b2f50684a36b6
demayuden <demayuden146@gmail.com>
2025-06-08 06:41:22 +0800

    whitenoise

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -141,3 +142,4 @@ REST_FRAMEWORK = {
     ]
 }
 ALLOWED_HOSTS = ['*']
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
